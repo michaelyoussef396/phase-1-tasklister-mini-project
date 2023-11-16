c0af4686ed9221503dd39e9f49a39c00e429ab33
michael youssef <michaelyoussef396@gmail.com>
2023-11-16 15:09:03 +1100

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form");
+  const tasksList = document.querySelector("#tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskInput = document.querySelector("#new-task-description");
+    const taskDescription = taskInput.value;
+
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskDescription;
+
+    tasksList.appendChild(taskItem);
+
+    taskInput.value = "";
+  });
 });
